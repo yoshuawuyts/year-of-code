1dbab07f93017540a9fd77f748fffbc31f9c0d66
Yoshua Wuyts <i@yoshuawuyts.com>
2015-05-03 19:13:17 +0200

    plot: inline-json -> csv
    plot: add csv data

--- plot/index.js
+++ plot/index.js
@@ -1,5 +1,9 @@
+const fromString = require('from2-string')
+const csv = require('csv-parser')
 const d3Tip = require('d3-tip')
+const path = require('path')
 const d3 = require('d3')
+const fs = require('fs')
 
 const d3tip = d3Tip(d3)
 
@@ -8,83 +12,82 @@ const marginX = 20
 const height = 300 - marginY * 2
 const width = 700 - marginX * 2
 
-const data = [
-  pt('website', 'year-of-code', 'https://yoshuawuyts.github.io/year-of-code', '2015-05-04'),
-  pt('module', 'from2-string','https://github.com/yoshuawuyts/from2-string', '2015-04-29'),
-  pt('post', 'Party tricks with ES6 symbols', 'https://medium.com/code-ops/party-tricks-with-es6-symbols-ee328fdb6c4b', '2015-04-30'),
-]
+const file = fs.readFileSync(path.join(__dirname, 'data.csv'), 'utf8')
+const data = []
+fromString(file)
+  .pipe(csv())
+  .on('data', d => data.push(pt(d)))
+  .on('end', createPlot)
 
-const xScale = d3.scale.linear()
-  .domain([0, 52])
-  .range([0, width])
+// create svg elements
+function createPlot () {
+  const xScale = d3.scale.linear()
+    .domain([0, 52])
+    .range([0, width])
 
-const yScale = d3.scale.linear()
-  .domain([0, 2])
-  .range([0, height])
+  const yScale = d3.scale.linear()
+    .domain([0, 2])
+    .range([0, height])
 
-const x = d3.time.scale()
-  .domain([new Date(2015, 4, 1), new Date(2016, 3, 1)])
-  .range([0, width])
+  const x = d3.time.scale()
+    .domain([new Date(2015, 4, 1), new Date(2016, 3, 1)])
+    .range([0, width])
 
-const xAxis = d3.svg.axis()
-  .scale(x)
-  .orient('bottom')
-  .ticks(d3.time.months)
-  .tickSize(16, 0)
-  .tickFormat(d3.time.format('%b'))
+  const xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom')
+    .ticks(d3.time.months)
+    .tickSize(16, 0)
+    .tickFormat(d3.time.format('%b'))
 
-const tip = d3tip()
-  .attr('class', 'plot-tip')
-  .html(d => '<span>' + d.name + '</span>')
+  const tip = d3tip()
+    .attr('class', 'plot-tip')
+    .html(d => '<span>' + d.name + '</span>')
 
-const svg = d3.select('[role="plot"]')
-  .append('svg')
-  .attr('height', height + marginY * 2)
-  .attr('width', width + marginX * 2)
-  .append('g')
-  .attr('transform', 'translate(' + marginX + ',' + marginY + ')')
-  .attr('class', 'plot')
+  const svg = d3.select('[role="plot"]')
+    .append('svg')
+    .attr('height', height + marginY * 2)
+    .attr('width', width + marginX * 2)
+    .append('g')
+    .attr('transform', 'translate(' + marginX + ',' + marginY + ')')
+    .attr('class', 'plot')
 
-svg.call(tip)
+  svg.call(tip)
 
-// create x axis
-svg.append('g')
-  .attr('transform', 'translate(0,' + (height + 20) + ')')
-  .attr('class', 'plot-x-axis')
-  .call(xAxis)
+  // create x axis
+  svg.append('g')
+    .attr('transform', 'translate(0,' + (height + 20) + ')')
+    .attr('class', 'plot-x-axis')
+    .call(xAxis)
 
-// create dots
-svg.selectAll('circle')
-   .data(data)
-   .enter()
-   .append('circle')
-   .attr('cx', d => xScale(d.cx))
-   .attr('cy', d => yScale(d.cy))
-   .attr('r', 7)
-   .on('mouseover', tip.show)
-   .on('mouseout', tip.hide)
-   .on('click', d => window.open(d.uri))
+  // create dots
+  svg.selectAll('circle')
+     .data(data)
+     .enter()
+     .append('circle')
+     .attr('cx', d => xScale(d.cx))
+     .attr('cy', d => yScale(d.cy))
+     .attr('r', 7)
+     .on('mouseover', tip.show)
+     .on('mouseout', tip.hide)
+     .on('click', d => window.open(d.uri))
 
-// add labels to dots
-svg.selectAll('text')
-  .data(data)
-  .enter()
-  .append('text')
-  .text(d => d.name)
-  .attr('x', d => xScale(d.cx))
-  .attr('y', d => yScale(d.cy))
+  // add labels to dots
+  svg.selectAll('text')
+    .data(data)
+    .enter()
+    .append('text')
+    .text(d => d.name)
+    .attr('x', d => xScale(d.cx))
+    .attr('y', d => yScale(d.cy))
+}
 
 // create a data point
-// str, str, str -> obj
-function pt (type, name, uri, date) {
-  return {
-    type: type,
-    name: name,
-    date: date,
-    uri: uri,
-    cx: parseWeek(date),
-    cy: typeIndex(type)
-  }
+// obj -> obj
+function pt (dat) {
+  dat.cx = parseWeek(dat.date)
+  dat.cy = typeIndex(dat.type)
+  return dat
 }
 
 // get the relative week from a date
